daa7a80d8e4fca13d10be14cbe5ab4f512145fc9
dousamat <dousamat@cvut.cz>
2025-09-28 19:21:06 +0200

    Add project files.

---
+++ SpotifyDataProcess/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace SpotifyDataProcess
+{
+    internal class Program
+    {
+        private static List<Record> filterResults(List<Record> records)
+        {
+            return records.Where(x =>
+                x.episode_name == null &&
+                x.episode_show_name == null &&
+                x.spotify_episode_uri == null &&
+                x.spotify_track_uri != null).ToList();
+        }
+        private static void printSeparator()
+        {
+            Console.WriteLine("=========================");
+        }
+        private static void processResults(List<Record> records)
+        {
+            long totalMinutes = records.Sum(x => x.ms_played ?? 0) /(1000 * 60);
+            var groupedSongs = records.Select(x => new { x.ms_played, x.master_metadata_track_name, x.master_metadata_album_artist_name })
+                                        .GroupBy(x => new { x.master_metadata_track_name, x.master_metadata_album_artist_name })
+                                        .Select(x => new
+                                        {
+                                            Song = x.Key.master_metadata_track_name,
+                                            Artist = x.Key.master_metadata_album_artist_name,
+                                            Playtime = x.Sum(s => s.ms_played)
+                                        }).ToList();
+            var groupedArtists = groupedSongs.GroupBy(x => x.Artist)
+                                        .Select(x => new
+                                        {
+                                            Artist = x.Key,
+                                            Playtime = x.Sum(s => s.Playtime)
+                                        }).ToList();
+            printSeparator();
+            Console.WriteLine("Total Song Records: " + records.Count);
+            Console.WriteLine("Total Listening Minutes: " + totalMinutes);
+            Console.WriteLine("Total Unique Songs: " + groupedSongs.Count);
+            printSeparator();
+            int topSongCount = 200;
+            Console.WriteLine("The list of top " + topSongCount + " songs:");
+            Console.WriteLine();
+            int topSongCount2 = topSongCount;
+            foreach (var song in groupedSongs.OrderByDescending(x => x.Playtime).Take(topSongCount).ToList())
+            {
+                Console.WriteLine((topSongCount2 - topSongCount + 1) + ": " + song.Artist + " - " + song.Song + " (" + (song.Playtime / (1000 * 60)) + ")");
+                topSongCount--;
+            }
+            printSeparator();
+            int topArtistCount = 50;
+            Console.WriteLine("The list of top " + topArtistCount + " artists:");
+            Console.WriteLine();
+            int topArtistCount2 = topArtistCount;
+            foreach (var artist in groupedArtists.OrderByDescending(x => x.Playtime).Take(topArtistCount).ToList())
+            {
+                Console.WriteLine((topArtistCount2 - topArtistCount + 1) + ": " + artist.Artist + " (" + (artist.Playtime / (1000 * 60)) + ")");
+                topArtistCount--;
+            }
+            printSeparator();
+        }
+        static void Main(string[] args)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string[] files = Directory.GetFiles(dir, "Streaming_History_Audio*.json");
+            Console.WriteLine("Files:");
+            int totalRecords = 0;
+            List<Record> database = new List<Record>();
+            foreach (string file in files)
+            {
+                Console.WriteLine("Processing: " + file);
+                var records = JsonSerializer.Deserialize<List<Record>>(File.ReadAllText(file));
+                if (records == null)
+                    throw new Exception("error parsing file: " + file);
+                totalRecords += records.Count;
+                database.AddRange(records);
+            }
+            Console.WriteLine("Total Records: " + totalRecords);
+            if (totalRecords != database.Count)
+                throw new Exception("wtf1");
+            processResults(filterResults(database));
+        }
+    }
+}
---
+++ SpotifyDataProcess/Record.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyDataProcess
+{
+    class Record
+    {
+        public DateTime ts { get; set; }
+        public string? username { get; set; }
+        public string? platform { get; set; }
+        public long? ms_played { get; set; }
+        public string? conn_country { get; set; }
+        public string? ip_addr_decrypted { get; set; }
+        public string? user_agent_decrypted { get; set; }
+        public string? master_metadata_track_name { get; set; }
+        public string? master_metadata_album_artist_name { get; set; }
+        public string? master_metadata_album_album_name { get; set; }
+        public string? spotify_track_uri { get; set; }
+        public string? episode_name { get; set; }
+        public string? episode_show_name { get; set; }
+        public string? spotify_episode_uri { get; set; }
+        public string? reason_start { get; set; }
+        public string? reason_end { get; set; }
+        public bool? shuffle { get; set; }
+        public bool? skipped { get; set; }
+
+        public bool? offline { get; set; }
+        public long? offline_timestamp { get; set; }
+        public bool? incognito_mode { get; set; }
+    }
+}
